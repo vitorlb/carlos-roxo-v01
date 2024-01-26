ba1eef4f06b5f1a482caf50336d4de03faf0f023
vitorlb <vitorlinhabranco@gmail.com>
2024-01-26 22:45:09 +0000

    resolve formData.js + order-info.php

--- js/formData.js
+++ js/formData.js
@@ -1,5 +1,16 @@
 let fetchedData = null;
+let contactForm = document.querySelector('#croxoContactForm')
 !!(typeof customFormData !== 'undefined') 
-    && (fetchedData = customFormData) 
+    && (fetchedData = customFormData); 
 
-console.log('all done', fetchedData)
+console.log('all done', fetchedData, contactForm)
+
+if(contactForm) {
+    debugger
+}
+
+/*
+!!contactForm && (
+  
+)
+*/
