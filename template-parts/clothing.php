a8eb1c4ec0f0e66ae8d3a0be53b8889b858b46fe
vitorlb <vitorlinhabranco@gmail.com>
2024-02-04 15:02:08 +0000

    Add clothing template parts

---
+++ template-parts/clothing.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * The template for displaying singular post-types: posts, pages and user-defined custom post types.
+ *
+ * @package HelloElementor
+ */
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+
+while (have_posts()) :
+	the_post();
+
+	// Attachment IDs
+	$images = get_post_meta(get_the_ID(), '_igmb_image_gallery_id', true);
+	$postTitle = trim(get_the_title());
+?>
+	<main id="content" class="container"> 
+		<?php get_template_part('template-parts/utils/js-data'); ?>
+		<div class="croxo-custom-post-type croxo-custom-post-type--illustrations d-flex">
+			<div class="page-content flextwo pt-4">
+				<?php get_template_part('template-parts/components/gallery'); ?>
+			</div>
+			<div class="croxo-custom-post-type__header flexone ps-4">
+			<?php get_template_part('template-parts/components/cpt-header-descr'); ?>
+			</div>
+			<?php comments_template(); ?>
+		</div>
+	</main>
+<?php
+endwhile;
